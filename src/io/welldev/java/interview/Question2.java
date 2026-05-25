56c0579a814821b4496a9ee247455e275d7853df
pranavseewoodharry <pranav.seewoodharry@welldev.io>
2026-05-25 15:47:32 +0400

    commit

--- src/io/welldev/java/interview/Question2.java
+++ src/io/welldev/java/interview/Question2.java
@@ -2,7 +2,6 @@
  * Write a Java method that accepts a List<String> of names and returns a List<String> containing only the names that appear more than 2 times in the input list.
  */
 void main() {
-    IO.println(addingNames());
 }
 
 List<String> name = new ArrayList<>();
